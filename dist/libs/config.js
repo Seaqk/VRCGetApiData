1209231681424cd65c1f483cf8ff058cf9cbedf5
AutoGHBot <178714696+AutoGHBot@users.noreply.github.com>
2024-11-30 14:56:37 +0000

    [Bot] JS compiled for the new version. See details: 9c0bc4af788234e5fbd980bd202a39a08744beff
    Co-authored-by: Saryniel <169302941+Saryniel@users.noreply.github.com>

--- dist/libs/config.js
+++ dist/libs/config.js
@@ -12,7 +12,7 @@ exports.cfg = {
     },
     data_folder: "data",
     web_api: {
-        redirect404: "https://github.com/Kyuddle/VRCGetApiData",
+        redirect404: "https://github.com/Nuiqka/VRCGetApiData",
         errorServer: "An internal server error has occurred. Please try again later.",
         groupFeature: "Group feature is disabled",
     },
